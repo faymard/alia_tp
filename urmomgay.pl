a2f266fa430e83be889174a5887dc577930ad52a
Ceruninco <48211045+Ceruninco@users.noreply.github.com>
2020-11-23 13:50:57 +0100

    working player versus player version

--- urmomgay.pl
+++ urmomgay.pl
@@ -1,6 +1,6 @@
+displayGrid(G,0).
 displayGrid(G,N):- N > 0, N1 is N-1,
                     write('|'), displayRow(G, N), displayGrid(G, N1).
-displayGrid(G,0).
 myNth1(N, C, X):- length(C, L), L >= N -> nth1(N,C,X) ; X=' '.
 
 displayRow([], N):- write('\n').
@@ -11,48 +11,55 @@ replaceInThePosition([_|T],1,E,[E|T]).
 replaceInThePosition([H|T],P,E,[H|R]) :-
     P > 1, NP is P-1, replaceInThePosition(T,NP,E,R).
 
-jouer:- jouerCoupO([[],[],[],[],[],[],[]]).
 addAtEnd(JOUEUR, COLONNE, NEWCOLONNE) :- append(COLONNE,[JOUEUR],NEWCOLONNE).
 
-saveMove(GRILLE, INDEX, JOUEUR) :- INDEX >0, INDEX =< 7, nth1(INDEX, GRILLE, COLONNE), length(COLONNE,L), L<6, write('old column : '),write(COLONNE), 
-		addAtEnd(JOUEUR, COLONNE, NEWCOLONNE), write(' ,new column : '),write(NEWCOLONNE),write('\n'), replaceInThePosition(GRILLE, INDEX, NEWCOLONNE, NEWGRILLE), write('nouvelle grille : '), write(NEWGRILLE).
+saveMove(GRILLE, INDEX, JOUEUR, NEWGRILLE) :- INDEX >0, INDEX =< 7, nth1(INDEX, GRILLE, COLONNE), length(COLONNE,L), L<6,
+		addAtEnd(JOUEUR, COLONNE, NEWCOLONNE), replaceInThePosition(GRILLE, INDEX, NEWCOLONNE, NEWGRILLE).
 		
-%winningPosition(GRILLE, JOUEUR) :- 
-
 isWinningColumn([], JOUEUR) :- false.
-isWinningColumn([COLUMN|RESTGRILLE], JOUEUR) :- write('column : '), write(COLUMN), sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],COLUMN).
+isWinningColumn([COLUMN|RESTGRILLE], JOUEUR) :- sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],COLUMN).
 isWinningColumn([COLUMN|RESTGRILLE], JOUEUR) :- isWinningColumn(RESTGRILLE, JOUEUR). 
 					 
 isWinningLine(GRILLE, JOUEUR ) :- isWinningLine(GRILLE, JOUEUR, 1).
-isWinningLine(GRILLE, JOUEUR, INDEX) :-  maplist(nth1(INDEX),GRILLE,LINE), sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR], LINE).
+isWinningLine(GRILLE, JOUEUR, INDEX) :-  maplist(elementOrEmpty(INDEX),GRILLE,LINE), sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR], LINE).
 isWinningLine(GRILLE, JOUEUR, INDEX) :-  INDEX <6, N is INDEX+1, isWinningLine(GRILLE, JOUEUR, N).
 
+elementOrEmpty(N,L,X):- nth1(N,L,X).
+elementOrEmpty(N, L, []):- length(L, LONG), LONG < N.
 
-isWinningDiag1(GRILLE,DIAG,JOUEUR,1):- write(DIAG),
-sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],DIAG).
 
-isWinningDiag1(GRILLE,DIAG,JOUEUR,N):- write(DIAG), N > 1,
-					  maplist(nth1(N), GRILLE, L),
-					  write('ur mom gay'),
-					  write(L),
-					  nth1(N,L,E),
+isWinningDiag1(GRILLE,DIAG,JOUEUR,0):- sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],DIAG).
+
+isWinningDiag1(GRILLE,DIAG,JOUEUR,N):- N > 0,
+					  maplist(elementOrEmpty(N), GRILLE, L),
+					  elementOrEmpty(N,L,E),
 					  N1 is N-1,
 					  isWinningDiag1(GRILLE,[E|DIAG],JOUEUR,N1).
 
 isWinningDiag1(GRILLE,JOUEUR):- isWinningDiag1(GRILLE,[],JOUEUR,7).
 
-isWinningDiag2(GRILLE,DIAG,JOUEUR,1):- write(DIAG),sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],DIAG).
-isWinningDiag2(GRILLE,DIAG,JOUEUR,N):- N > 1,
-					  maplist(nth1(N), GRILLE, L),
+isWinningDiag2(GRILLE,DIAG,JOUEUR,0):- sublist([JOUEUR,JOUEUR,JOUEUR,JOUEUR],DIAG).
+isWinningDiag2(GRILLE,DIAG,JOUEUR,N):- N > 0,
+					  maplist(elementOrEmpty(N), GRILLE, L),
 					  N2 is 8-N,
-					  nth1(N2,L,E),
+					  elementOrEmpty(N2,L,E),
 					  N1 is N-1,
 					  isWinningDiag2(GRILLE,[E|DIAG],JOUEUR,N1).
 
 isWinningDiag2(GRILLE,JOUEUR):- isWinningDiag2(GRILLE,[],JOUEUR,7).
 
-% nextStep
-% winningPosition
-% fullGrid
-% nextPlayer
+winningPosition(GRILLE, JOUEUR) :- isWinningLine(GRILLE, JOUEUR) ; isWinningColumn(GRILLE, JOUEUR) ;  isWinningDiag1(GRILLE, JOUEUR) ; isWinningDiag2(GRILLE, JOUEUR). 
+
+fullGrid([]).
+fullGrid([HEAD|SUBGRILLE]) :- length(HEAD,7), fullGrid(SUBGRILLE).
+
+nextStep(GRILLE, JOUEUR) :- fullGrid(GRILLE), write('complete grid, it\'s a draw\n').
+nextStep(GRILLE, JOUEUR) :- winningPosition(GRILLE, JOUEUR), write('Joueur '), write(JOUEUR), write(' won, game over').
+nextStep(GRILLE, 'x') :- play(GRILLE,'o').
+nextStep(GRILLE, 'o') :- play(GRILLE,'x').
+
+play(GRILLE, JOUEUR) :- write(JOUEUR), write(' player, enter a valid column number N (1<=N<=7) : '), read(N), saveMove(GRILLE, N, JOUEUR, NEWGRILLE), displayGrid(NEWGRILLE,7),write('\n'),nextStep(NEWGRILLE,JOUEUR).
+
+
+start :- nextStep([[],[],[],[],[],[],[]],'x').
 
