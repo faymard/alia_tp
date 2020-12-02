a505d3c03455cc35cc533d9556038fce5bafc3c0
HuchotP <32591037+HuchotP@users.noreply.github.com>
2020-12-02 09:16:02 +0100

    ajout d'une heuristique

--- urmomgay.pl
+++ urmomgay.pl
@@ -81,16 +81,30 @@ playIARandom(GRILLE, JOUEUR):- random_between(1,7, N), nth1(N, GRILLE, X), lengt
 %%%%%%%%%%%%%%%%% MIN MAX %%%%%%%%%%%
 % chercher meilleur coup -> chercher meilleur coup pour l'adversaire -> chercher coup gagnant
 
-% cas où la profondeur est impaire : on cherche à minimiser le prochain coup de l'adversaire
-playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR):- PROFONDEUR mod 2 == 0, 
+% cas où la profondeur est paire : on cherche à minimiser le prochain coup de l'adversaire
+playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR):- PROFONDEUR mod 2 == 0.
 
-% cas où la profondeur est paire : on cherche à maximiser le coup joué
-playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR):- PROFONDEUR mod 2 == 1, 
+% cas où la profondeur est impaire : on cherche à maximiser le coup joué
+playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR):- PROFONDEUR mod 2 == 1.
 
-playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR, COLONNE):- COLONNE < 8, 
+playKindOfMinMax(GRILLE, JOUEUR, PROFONDEUR, COLONNE):- COLONNE < 8.
 
 playHypotheticalWinningMove(GRILLE, JOUEUR, COLONNE, NEWGRILLE):- COLONNE < 8, nth1(COLONNE, GRILLE, X), length(X, L), L < 6, saveMove(GRILLE, COLONNE, JOUEUR, NEWGRILLE), 
 										winningPosition(NEWGRILLE, JOUEUR).
+minimax(GRILLE, PROFONDEUR):-minimax(GRILLE, PROFONDEUR, 1).
+
+minimax(GRILLE, PROFONDEUR, COLONNE):- PROFONDEUR > 0, COLONNE < 4, write(PROFONDEUR), write(' '), write(COLONNE), write('\n'), P is PROFONDEUR - 1, minimax(GRILLE, P, COLONNE), C is COLONNE + 1, minimax(GRILLE, PROFONDEUR, C).
+%minimax(GRILLE, PROFONDEUR, COLONNE):- PROFONDEUR > 0, COLONNE == 3, write(PROFONDEUR), write(' '), write(COLONNE), write('\n'), P is PROFONDEUR + 1, minimax(GRILLE, P , 1).
+%minimax(GRILLE, PROFONDEUR, COLONNE):- PROFONDEUR == 0, COLONNE < 3, write(PROFONDEUR), write(' '), write(COLONNE), write('\n'),P is PROFONDEUR + 1, C is COLONNE + 1, minimax(GRILLE, P, C).
+minimax(GRILLE, PROFONDEUR, COLONNE):- PROFONDEUR == 1, write('end node\n').
+minimax(GRILLE, PROFONDEUR, COLONNE).
+
+
+%score du jeton ajouté à la position colonne
+
+score(GRILLE, COLONNE, SCORE):- score(GRILLE, COLONNE, SCORE, [[3,4,5,5,4,3], [4,6,8,8,6,4],[5,8,11,11,8,5], [7,10,13,13,10,7], [5,8,11,11,8,5], [4,6,8,8,6,4],[3,4,5,5,4,3]]).
+
+score(GRILLE, COLONNE, SCORE, HEURISTIQUE):- nth1(COLONNE, GRILLE, X), length(X, L), nth1(COLONNE, HEURISTIQUE, Y), nth0(L, Y, SCORE).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 playWinningMove(GRILLE, JOUEUR):- playWinningMove(GRILLE, JOUEUR, 1).
